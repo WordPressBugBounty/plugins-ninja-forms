f317097fc08a8502a42c68377be180be0859358a
Sean Murphy <sean@iamseanmurphy.com>
2026-08-31 20:01:53 +0000

    3.15.2

--- ninja-forms/vendor/composer/installed.php
+++ ninja-forms/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'saturday-drive/ninja-forms',
-        'pretty_version' => 'dev-63d76b90d38104d37287a0feeec2f13ec6198dfd',
-        'version' => 'dev-63d76b90d38104d37287a0feeec2f13ec6198dfd',
-        'reference' => '63d76b90d38104d37287a0feeec2f13ec6198dfd',
+        'pretty_version' => 'dev-e96501b8c2604fa476aa6564a814537728b8c1e9',
+        'version' => 'dev-e96501b8c2604fa476aa6564a814537728b8c1e9',
+        'reference' => 'e96501b8c2604fa476aa6564a814537728b8c1e9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'saturday-drive/ninja-forms' => array(
-            'pretty_version' => 'dev-63d76b90d38104d37287a0feeec2f13ec6198dfd',
-            'version' => 'dev-63d76b90d38104d37287a0feeec2f13ec6198dfd',
-            'reference' => '63d76b90d38104d37287a0feeec2f13ec6198dfd',
+            'pretty_version' => 'dev-e96501b8c2604fa476aa6564a814537728b8c1e9',
+            'version' => 'dev-e96501b8c2604fa476aa6564a814537728b8c1e9',
+            'reference' => 'e96501b8c2604fa476aa6564a814537728b8c1e9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
